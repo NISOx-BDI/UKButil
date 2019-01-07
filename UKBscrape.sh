9a51ad521d3a3835cae0736aa5000803b5b830ee
nicholst <thomas.nichols@bdi.ox.ac.uk>
2019-01-07 13:42:09 +0000

    Adding UKBscrape.sh

---
+++ UKBscrape.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -x
+
+URL=(\
+     'http://biobank.ctsu.ox.ac.uk/crystal/list.cgi?it=0&vt=11' \
+	 'http://biobank.ctsu.ox.ac.uk/crystal/list.cgi?it=0&vt=21' \
+	 'http://biobank.ctsu.ox.ac.uk/crystal/list.cgi?it=0&vt=22' \
+	 'http://biobank.ctsu.ox.ac.uk/crystal/list.cgi?it=0&vt=31' \
+	 'http://biobank.ctsu.ox.ac.uk/crystal/list.cgi?it=0&vt=41' \
+	 'http://biobank.ctsu.ox.ac.uk/crystal/list.cgi?it=0&vt=51' \
+	 'http://biobank.ctsu.ox.ac.uk/crystal/list.cgi?it=0&vt=61' \
+	 'http://biobank.ctsu.ox.ac.uk/crystal/list.cgi?it=0&vt=101' )
+Type=( 'Integer' \
+	   'CategoricalSingle' \
+	   'CategoricalMultiple' \
+	   'Continuous' \
+	   'Text' \
+	   'Date' \
+	   'Time' \
+	   'Compound' )
+
+cp /dev/null IDs_All.txt
+
+for ((i=0;i<${#URL[@]};i++)) ; do
+    wget -O /tmp/$$ "${URL[i]}"
+    grep 'field.cgi?id=' /tmp/$$ | sed 's/^.*field.cgi?id=\([0-9]*\).*$/\1/' > "IDs_${Type[i]}.txt"
+    cat IDs_${Type[i]}.txt >> IDs_All.txt
+done
+
+	 
+
+    
