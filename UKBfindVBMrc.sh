e30bfc90362452691340ec64d763b2249ff0d1d7
nicholst <thomas.nichols@bdi.ox.ac.uk>
2019-02-14 10:24:14 +0000

    Speed up VBM find

--- UKBfindVBMrc.sh
+++ UKBfindVBMrc.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-dirSubjList="${UKB_SMS}/ukb25120-VBM-6k.txt"
+dirSubjList="${UKB_SMS}/ID-eid8107_VBM-6k.txt"
 dirSubjs="${UKB_SUBJECTS}/subjects4"
 
 cd $dirSubjs
 
-find -L . \! -readable -prune -o -path "*/T1/T1_vbm/T1_GM_to_template_GM_mod.nii.gz" -print > $dirSubjList
-
-
+# First two path/prune's are important to stop find wasting time in non-T1 directories
+find -L . \
+    \! -readable -prune -o \
+    -path '*/T2*' -prune -o \
+    -path '*/[lfSdIQ]*' -prune -o \
+    -path "*/T1/T1_vbm/T1_GM_to_template_GM_mod.nii.gz" -print > $dirSubjList
