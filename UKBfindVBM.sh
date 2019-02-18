eb69acccea828a25ab301eb59c7eef39c0398569
Tom Nichols <thomas.nichols@bdi.ox.ac.uk>
2019-02-18 08:29:30 +0000

    Adding updates from UKBfindVBMrc.sh

--- UKBfindVBM.sh
+++ UKBfindVBM.sh
@@ -7,6 +7,10 @@ dirSubjs="/vols/Data/ukbiobank/FMRIB/IMAGING/data3/SubjectsAll"
 
 cd $dirSubjs
 
-find -L . \! -readable -prune -o -path "*/T1/T1_vbm/T1_GM_to_template_GM_mod.nii.gz" -print > $dirSubjList
+find -L . \! -readable -prune -o \
+    -path '*/T2*' -prune -o \
+    -path '*/[lfSdIQ]*' -prune -o \
+    -path "*/T1/T1_vbm/T1_GM_to_template_GM_mod.nii.gz" -print | \
+    > $dirSubjList
 
 
