d82d1846557da26c9dc4a525674010a7ea60f1e3
Tom Nichols <thomas.nichols@bdi.ox.ac.uk>
2019-02-18 09:29:44 +0000

    Fix pipe bug

--- UKBfindVBM.sh
+++ UKBfindVBM.sh
@@ -10,7 +10,7 @@ cd $dirSubjs
 find -L . \! -readable -prune -o \
     -path '*/T2*' -prune -o \
     -path '*/[lfSdIQ]*' -prune -o \
-    -path "*/T1/T1_vbm/T1_GM_to_template_GM_mod.nii.gz" -print | \
+    -path "*/T1/T1_vbm/T1_GM_to_template_GM_mod.nii.gz" -print \
     > $dirSubjList
 
 
