0c5403f4249cce6d4e7c28defdf5c34d612943fa
nicholst <thomas.nichols@bdi.ox.ac.uk>
2020-04-11 15:26:05 +0100

    Complete UKBwarp_tfMRI to record bridged ids

--- UKBfindtfMRI.sh
+++ UKBfindtfMRI.sh
@@ -3,30 +3,30 @@
 dirSubjImg="${UKB_SMS}/FN-eid8107_tfMRI-6k.txt"
 dirSubjIDsSS="${UKB_SMS}/IDs-eid8107_tfMRI-6k.txt"
 dirSubjIDsSS1="${UKB_SMS}/IDs-eid8107_tfMRI-1k.txt"
+dirSubjIDsSS5="${UKB_SMS}/IDs-eid8107_tfMRI-5k.txt"
 dirSubjIDsTN="${UKB_SMS}/IDs-eid34077_tfMRI-6k.txt"
 dirSubjs="${UKB_SUBJECTS}/subjects4"
 tmp=${UKB_SMS}/tmp$$
 
 cd $dirSubjs
 
-# First two path/prune's are important to stop find wasting time in non-T1 directories
 ls -1 */fMRI/tfMRI.fsf > $dirSubjImg
 
 awk -F/ '{print $1}' ${dirSubjImg} > ${dirSubjIDsSS}
 
 shuf ${dirSubjIDsSS} | head -n 1000 > ${dirSubjIDsSS1}
+comm -3 <(sort ${dirSubjIDsSS}) <(${dirSubjIDsSS1}) ${dirSubjIDsSS1} | tr -d '\t' > ${dirSubjIDsSS5}
 
+funpack \
+    $tmp \
+    $dirSubjIDsSS \
+    ${UKB_SMS}/bridge_8107_34077.tsv
 
-# funpack \
-#     $tmp \
-#     $dirSubjIDsSS \
-#     ${UKB_SMS}/bridge_8107_34077.tsv
+# Create plain text list of (our, TN) IDs
+awk -F'\t' '(NR>1){print $2}' \
+    $tmp > \
+    $dirSubjIDsTN
 
-# # Create plain text list of (our, TN) IDs
-# awk -F'\t' '(NR>1){print $2}' \
-#     $tmp > \
-#     $dirSubjIDsTN
-
-# rm $tmp
+rm $tmp
 
 
