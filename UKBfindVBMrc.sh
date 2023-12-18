6ecd7584d15eb79ee339dd493f4cfde53bee98b6
nicholst <thomas.nichols@bdi.ox.ac.uk>
2023-12-18 13:17:51 +0000

    Update UKBfindVBMrc.sh

--- UKBfindVBMrc.sh
+++ UKBfindVBMrc.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
 
-dirSubjImg="${UKB_SMS}/FN-eid8107_VBM-6k.txt"
-dirSubjIDsSS="${UKB_SMS}/IDs-eid8107_VBM-6k.txt"
-dirSubjIDsTN="${UKB_SMS}/IDs-eid34077_VBM-6k.txt"
-dirSubjs="${UKB_SUBJECTS}/subjects4"
+dirSubjs="${UKB_SUBJECTS}/subjectsAll"
+
+dirSubjImg="${UKB_SMS}/FN-eid8107_VBM-72k.txt"
+dirSubjIDsSS="${UKB_SMS}/IDs-eid8107_VBM-72k.txt"
+dirSubjIDsTN="${UKB_SMS}/IDs-eid34077_VBM-72k.txt"
+
 tmp=${UKB_SMS}/tmp$$
 
 cd $dirSubjs
 
-# First two path/prune's are important to stop find wasting time in non-T1 directories
-find -L . \
-    \! -readable -prune -o \
-    -path '*/T2*' -prune -o \
-    -path '*/[lfSdIQ]*' -prune -o \
-    -path "*/T1/T1_vbm/T1_GM_to_template_GM_mod.nii.gz" -print > \
-    ${dirSubjImg}
+cp /dev/null $dirSubjImg
+for i in {0..9} ; do
+    for j in {0..9} ; do
+	echo -n "."
+	ls -1 *${i}${j}/T1/T1_vbm/T1_GM_to_template_GM_mod.nii.gz >> $dirSubjImg
+    done
+done
+echo " done."
 
-awk -F/ '{print $2}' ${dirSubjImg} > ${dirSubjIDsSS}
+awk -F/ '{print $1}' ${dirSubjImg} >> ${dirSubjIDsSS}.txt
+echo "eid" > ${dirSubjIDsSS}.csv
+cat ${dirSubjIDsSS}.txt >> ${dirSubjIDsSS}.csv
 
-# Create temp briding file just for these subjects
-ukbparse --no_builtins --overwrite \
-    $tmp \
-    $dirSubjIDsSS \
-    ${UKB_SMS}/bridge_8107_34077.tsv
+
+module load Anaconda3/2023.07-2
+eval "$(conda shell.bash hook)"
+conda activate py310
+
+fmrib_unpack \
+    ${tmp}.tsv \
+    ${dirSubjIDsSS}.csv \
+    ${UKB_SMS}/bridge_8107_34077-long.tsv
 
 # Create plain text list of (our, TN) IDs
 awk -F'\t' '(NR>1){print $2}' \
-    $tmp > \
-    $dirSubjIDsTN
+    ${tmp}.tsv > \
+    ${dirSubjIDsTN}.txt
 
 rm $tmp
-
 
